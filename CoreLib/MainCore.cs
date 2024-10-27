b76cb513c0ef8ce1d126afe25b97ea672a460f3b
M. Fitri <42993717+mfitri-p3@users.noreply.github.com>
2024-10-28 01:29:34 +0800

    Visual Studio Solution init

---
+++ CoreLib/MainCore.cs
@@ -0,0 +1,59 @@
+﻿namespace CoreLib
+{
+    /// <summary>
+    /// Core library of the project.
+    /// </summary>
+    public class MainCore
+    {
+        #region Fields and Properties
+
+        private readonly string _baseApiUrl = "https://api.data.gov.my/";
+
+        private static readonly MainCore _instance = new MainCore();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string UrlSource_MainSite
+        {
+            get
+            {
+                return "https://data.gov.my/";
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string UrlSource_ApiDoc
+        {
+            get
+            {
+                return "https://developer.data.gov.my/";
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        private MainCore()
+        {
+
+        }
+
+        /// <summary>
+        /// Get singleton instance of MainCore.
+        /// </summary>
+        /// <returns></returns>
+        public static MainCore Instance() { return _instance; }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pasaraya-harga-tengok-MYR
-Price lookup and comparison among supermarkets in Malaysia
+
+Price lookup and comparison of common goods among supermarkets in Malaysia
+
+Uses [data.gov.my](https://data.gov.my/) as API source.
+
+#### Disclaimer
+
+This project is developed for educational purpose.
